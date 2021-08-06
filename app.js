de3dce857ca9bc8a9e8748bfe49c2fd75806617c
Thomas <65050898+tvonment@users.noreply.github.com>
2021-08-06 17:50:48 +0000

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.get("/heroes", (req,res) => {
     const heroes = [
